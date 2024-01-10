663d200dd7efe82bc4a0f1eaa3e93080c1e72679
EriikGabriel <erikgabriel.lins@hotmail.com>
2024-01-09 22:02:13 -0300

    feat: basic configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          "50": "#fff7ec",
+          "100": "#ffeed3",
+          "200": "#ffd9a5",
+          "300": "#ffbe6d",
+          "400": "#ff9632",
+          "500": "#ff770a",
+          "600": "#f55a00",
+          "700": "#cc4202",
+          "800": "#a1340b",
+          "900": "#822d0c",
+          "950": "#461404",
+        },
       },
     },
   },
