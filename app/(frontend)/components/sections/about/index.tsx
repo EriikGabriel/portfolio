9338507b5cb81cfbdc8ca3de3cc80cc1967aeaa9
EriikGabriel <erikgabriel.lins@hotmail.com>
2026-08-14 00:03:39 -0300

    build: fix build process

--- app/(frontend)/components/sections/about/index.tsx
+++ app/(frontend)/components/sections/about/index.tsx
@@ -46,7 +46,7 @@ export async function About() {
 							<h1 className="text-bright-primary text-5xl">{about.greeting}</h1>
 							<p className="pr-5 font-geist text-xl font-normal tracking-tight">
 								{about.description}{" "}
-								{about.techs.map((tech, i) => (
+								{about?.techs?.map((tech, i) => (
 									<span key={tech.id}>
 										<a key={tech.id} className="text-primary" href={tech.url}>
 											{tech.name}
